b2a3b2839115ec728e619fe3b468b19e3fddc147
yijian <yijian@qunhemail.com>
2021-01-27 00:00:31 +0800

    优化

--- src/main/java/icons/SdkIcons.java
+++ src/main/java/icons/SdkIcons.java
@@ -8,6 +8,6 @@ import javax.swing.*;
 
 public class SdkIcons {
 
-  public static final Icon Sdk_default_icon = IconLoader.getIcon("/icons/sdk_16.svg");
+  public static final Icon Sdk_default_icon = IconLoader.getIcon("/icons/jiaohu.svg");
 
 }
